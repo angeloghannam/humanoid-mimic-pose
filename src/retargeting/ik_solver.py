59c4ee7c0d7df278f1d313a8a63d8d5e1188e7e1
Marc Aurele <marc-aurele.ferraz@ipsa.fr>
2026-03-09 10:56:04 +0100

    WIP IK solver

---
+++ src/retargeting/ik_solver.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Forward_Kinematics:
+    def __init__(self, joints, joints_limits, offsets, parents=None, axes=None):
+        self.joints = joints
+        self.joints_limits = joints_limits
+        self.offsets = offsets
+        self.parents = parents
+        self.axes = axes
+    def compute_fk(q):
+        pass
+    def compute_jacobian():
+        pass
+    def verif():
+        pass
+    
